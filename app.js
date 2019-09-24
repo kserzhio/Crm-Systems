406326075fdd11ae2a6a8ceb874804a0b9e2c757
kserzhio <kserzhio@gmail.com>
2019-09-24 20:17:09 +0300

    add routes and controllers && cors and morgan

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const  express = require('express');
-const authRoutes = require('./routes/auth')
+const bodyParser  = require('body-parser');
+const authRoutes = require('./routes/auth');
+const analyticsRoutes = require('./routes/analitics');
+const categoryRoutes = require('./routes/category');
+const orderRoutes = require('./routes/order');
+const positionRoutes = require('./routes/position');
 const app = express();
 
+
+app.use(require('morgan')('dev'))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(require('cors')());
+
 app.use('/api/auth',authRoutes)
+app.use('/api/analytics',analyticsRoutes)
+app.use('/api/category',categoryRoutes)
+app.use('/api/order',orderRoutes)
+app.use('/api/position',positionRoutes)
 
 module.exports = app;
