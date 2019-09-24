ec5d0c092f0526ca04c756aa48d290ce4bde6404
kserzhio <kserzhio@gmail.com>
2019-09-24 21:35:18 +0300

    register user &&  controller check user email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/User')
+
 module.exports.login = function (req,res) {
     res.status(200).json({
         login:{
@@ -7,8 +9,13 @@ module.exports.login = function (req,res) {
     })
 }
 
-module.exports.register = function (req,res) {
-    res.status(200).json({
-        register: 'From Controller'
-    })
+module.exports.register = async function (req,res) {
+    const candidate = await User.findOne({email:req.body.email})
+    if(candidate){
+        res.status(409).json({
+            message:'This email has been error'
+        })
+    } else {
+
+    }
 }
