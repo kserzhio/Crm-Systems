18849030bf0751b7c585448062cbf0c826e6cc6a
kserzhio <kserzhio@gmail.com>
2019-09-25 20:22:01 +0300

    add bcrypt and jwt token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,32 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../models/User')
+const keys = require('../config/keys')
 
-module.exports.login = function (req,res) {
-    res.status(200).json({
-        login:{
-            email: req.body.email,
-            password: req.body.password
+module.exports.login = async function (req,res) {
+    const candidate = await User.findOne({email:req.body.email})
+
+    if(candidate){
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+        if(passwordResult){
+            const token = jwt.sign({
+                email:candidate.email,
+                userId:candidate._id
+            },keys.jwt,{expiresIn: 60 * 60})
+
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+        } else {
+            res.status(401).json({
+                message:'Password incorrect'
+            })
         }
-    })
+    } else {
+       res.status(404).json({
+           message:'User not found.'
+       })
+    }
 }
 
 module.exports.register = async function (req,res) {
@@ -16,6 +36,18 @@ module.exports.register = async function (req,res) {
             message:'This email has been error'
         })
     } else {
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+            const user = new User({
+                email:req.body.email,
+                password: bcrypt.hashSync(password,salt)
+            })
+        try {
+            await user.save()
+            res.status(201).json(user)
 
+        } catch (e) {
+
+        }
     }
 }
