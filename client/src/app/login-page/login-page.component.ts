cbd8b3415543f926124bd3d156dd45e3efc83264
kserzhio <kserzhio@gmail.com>
2019-10-01 21:48:50 +0300

    add auth service

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,22 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../shared/services/auth.service";
+import {Subscription} from "rxjs";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.less']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit,OnDestroy {
   form: FormGroup
-  constructor() { }
+  aSub:Subscription
+  constructor(private auth:AuthService,
+              private router:Router,
+              private route:ActivatedRoute) { }
 
   ngOnInit() {
     this.form = new FormGroup({
       email:new FormControl(null,[Validators.required,Validators.email]),
       password:new FormControl(null,[Validators.required,Validators.minLength(6)])
     })
-  }
-  onSubmit(){
+    this.route.queryParams.subscribe((params:Params)=>{
+       if(params['registered']){
 
+       } else if(params['accessDenied']){
+
+       }
+    })
+  }
+  ngOnDestroy(){
+    if(this.aSub){
+      this.aSub.unsubscribe()
+    }
+  }
+
+  onSubmit(){
+    this.form.disable()
+     this.aSub = this.auth.login(this.form.value).subscribe(
+          () => this.router.navigate(['/overview']),
+          error => {
+              console.log(error)
+            this.form.enable()
+          }
+      )
   }
 }
